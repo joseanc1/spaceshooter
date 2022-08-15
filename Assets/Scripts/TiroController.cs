ad6e6266ddbf0ccc754cd10cac7f5dff9f27207a
joseanc1 <joseancoelho_@hotmail.com>
2022-08-15 17:49:40 -0300

    script tiro player

---
+++ Assets/Scripts/TiroController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroController : MonoBehaviour
+{
+    private Rigidbody2D meuRB;
+
+    [SerializeField] private float vel = 10f;
+    
+    
+    void Start()
+    {
+        
+        //pegando o rigidbody
+        meuRB = GetComponent<Rigidbody2D>();
+        
+        //indo para cima
+        meuRB.velocity = new Vector2(0f, vel);
+
+
+    }
+
+    
+    void Update()
+    {
+      
+    }
+}
