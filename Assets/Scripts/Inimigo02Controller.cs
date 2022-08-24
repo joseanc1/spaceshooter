a6e0b1a1ae81d62f686ecfd019bbf4f769fca6d3
joseanc1 <joseancoelho_@hotmail.com>
2022-08-24 12:19:22 -0300

    criação inimigo2, script, gameobject, animation, prefab, tag, collider e rigidbody

---
+++ Assets/Scripts/Inimigo02Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo02Controller : InimigoPai
+{
+    //rigidbody
+    [SerializeField] private Rigidbody2D meuRb;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
